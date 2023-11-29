9d9371c16fd123fa58411c410d2a4d3b35232f24
20103551 <20103551@mail.wit.ie>
2023-11-29 11:57:20 +0000

    Add updateQuestions

--- src/main/kotlin/models/Rounds.kt
+++ src/main/kotlin/models/Rounds.kt
@@ -15,12 +15,43 @@ data class Rounds(
 
     fun numberOfQuestions() = questions.size
 
-    fun findOne(id: Int): Questions?{
-        return questions.find{ question -> question.questionId == id }
+    fun findOne(id: Int): Questions? {
+        return questions.find { question -> question.questionId == id }
     }
 
-    fun delete(id: Int): Boolean {
-        return questions.removeIf { question -> question.questionId == id}
+    fun deleteQuestions(id: Int): Boolean {
+        return questions.removeIf { question -> question.questionId == id }
     }
+
+
+    fun updateQuestions(id: Int, newQuestions: Questions): Boolean {
+        val foundQuestions = findOne(id)  //if the object exists, use the details passed in the newItem parameter to
+        //update the found object in the Set
+        if (foundQuestions != null) {
+            foundQuestions.questionId = newQuestions.questionId
+            foundQuestions.questionText = newQuestions.questionText
+            foundQuestions.correctAnswer = newQuestions.correctAnswer
+            foundQuestions.answer = newQuestions.answer
+            return true
+        }
+
+        //if the object was not found, return false, indicating that the update was not successful
+        return false
+    }
+
+    /*fun updateQuestionsId(id: Int, newQuestions: Questions): Boolean {
+        val foundQuestions = findOne(id)  //if the object exists, use the details passed in the newItem parameter to
+        //update the found object in the Set
+        if (foundQuestions != null) {
+            foundQuestions.itemContents = newQuestions.questionContents
+            foundQuestions.isItemComplete = newQuestions.isItemComplete
+            return true
+        }
+
+        //if the object was not found, return false, indicating that the update was not successful
+        return false
+    }
+    change how update works later.
+    */
 
 }
